77f08bbc0dca9fcf2f5012e710be99cc2610f711
Stelios Anastasakis <bpstelios10@hotmail.com>
2023-05-24 00:47:43 +0300

    [events-feature] added spring profiles option

--- service/scripts/startup.sh
+++ service/scripts/startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 
+if [[ -z $DEPLOYMENT_ENVIRONMENT ]]
+then
+  echo -n "Error: \$DEPLOYMENT_ENVIRONMENT is not set."
+  echo " If using plain Docker, specify '--env DEPLOYMENT_ENVIRONMENT=<environment-name>'"
+  exit 2
+fi
+
+export SPRING_PROFILES_ACTIVE=$DEPLOYMENT_ENVIRONMENT
 export JAVA_OPTIONS="-XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=32m -XX:ReservedCodeCacheSize=64m -XX:+UnlockExperimentalVMOptions -Xmx512M -Xms512M -Xss256k $EXTRA_JAVA_OPTIONS"
 export GC_OPTIONS="-XX:+UseG1GC"
 
